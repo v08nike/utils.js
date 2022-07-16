11684da52914132794f46161a6a4f5aa290fba12
v08nike <teddan817@gmail.com>
2022-07-16 14:06:15 +0600

    ...

--- index.ts
+++ index.ts
@@ -7,14 +7,20 @@ interface Document {
   qsa(selector: String): Element[]
 }
 interface Array<T> {
-  random(): any
-  sortByKey(key: string, sort: any): []
+  randomElement(): any
+  sortByKey(key: string, sort: [Boolean, number]): []
+}
+interface Math {
+  randomNumber(min: number, max: number): number
 }
 interface Object {
   forEach(callback: Function): void
 }
 interface Window {
   HTML(body: string): Element
+  fetchJSON(url: [String, URL], opt: Object): any
+  fetchTEXT(url: [String, URL], opt: Object): any
+  Wait(time: number): any
 }
 
 Document.prototype.qs = function () {
@@ -33,11 +39,11 @@ Element.prototype.qsa = function () {
   return [...this.querySelectorAll.apply(this, arguments)]
 }
 
-Array.prototype.random = function () {
+Array.prototype.randomElement = function () {
   return this[Math.floor(Math.random() * this.length)]
 }
 
-Array.prototype.sortByKey = function (key, sort): [] {
+Array.prototype.sortByKey = function (key, sort) {
   if (!sort) return this.sort((a, b) => (a[key] > b[key] ? 1 : b[key] > a[key] ? -1 : 0))
   return this.sort((b, a) => (a[key] > b[key] ? 1 : b[key] > a[key] ? -1 : 0))
 }
@@ -48,6 +54,25 @@ Object.prototype.forEach = function (callback) {
   })
 }
 
-window.HTML = (body = "<div></div>") => {
+Math.randomNumber = function (min = 0, max = 1) {
+  if (arguments.length === 1) (max = min), (min = 0)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+window.HTML = function (body = "<div></div>") {
   return new DOMParser().parseFromString(body, "text/html").body.firstElementChild
+}
+
+window.fetchJSON = async function () {
+  return await (await fetch.apply(this, arguments)).json()
+}
+
+window.fetchTEXT = async function () {
+  return await (await fetch.apply(this, arguments)).text()
+}
+
+window.Wait = function (duration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration)
+  })
 }
